2d65b31bcb92ec4a8bca87d0e2e96f458431fcfb
invisloa <91217120+invisloa@users.noreply.github.com>
2023-06-24 22:42:15 +0200

    Add project files.

---
+++ WeatherCalendar/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherCalendar;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ WeatherCalendar/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WeatherCalendar;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
